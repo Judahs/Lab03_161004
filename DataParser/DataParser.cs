058721423773172c2fb64dbb987e6c4d89421da0
salmodesign <salmodesign@gmail.com>
2016-10-06 18:58:11 +0200

    Comments/summary added for CreateDataToSaveFromList

--- DataParser/DataParser.cs
+++ DataParser/DataParser.cs
@@ -13,11 +13,13 @@ namespace lab03
             List <IVehicle> testList = new List<IVehicle>();
             return testList;
         }
+        
         /// <summary>
-        /// 
+        /// Creates a list of strings. Loops through all vehicles and first stores the type and
+        /// then the name and the speed, separated with semi-colon.
         /// </summary>
-        /// <param name="_allVehicles"></param>
-        /// <returns></returns>
+        /// <param name="_allVehicles"> A list of all Vehicles from program, of type that implements the IVehicle interface</param>
+        /// <returns>A list of strings that can be used in other class/assembly to save the data in a file.</returns>
         public List<string> CreateDataToSaveFromList(IEnumerable<IVehicle> _allVehicles)
         {
             List<string> stringList = new List<string>();
